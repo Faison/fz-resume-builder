4a657808f2974d82c9ad8ebd997bab7200fad510
Faison Zutavern <fzutavern@my.wctc.edu>
2016-12-07 13:23:21 -0600

    Fixed the FZ_RESUME_URL variable

--- fz-resume.php
+++ fz-resume.php
@@ -9,7 +9,7 @@ namespace FZ_Resume;
 
 // Useful global constants
 define( 'FZ_RESUME_VERSION', '0.1.0' );
-define( 'FZ_RESUME_URL',     plugin_dir_url( __FILE__ ) );
+define( 'FZ_RESUME_URL',     get_template_directory_uri() . '/includes/fz-resume/' );
 define( 'FZ_RESUME_PATH',    dirname( __FILE__ ) . '/' );
 define( 'FZ_RESUME_INC',     FZ_RESUME_PATH . 'includes/' );
 
