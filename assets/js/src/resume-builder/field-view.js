c00b3f1b73e7b38e2e1af93526d3ba3a369f71cf
Faison Zutavern <fzutavern@my.wctc.edu>
2016-12-07 22:39:38 -0600

    Moved the Field_View functions to private, component level functions

--- assets/js/src/resume-builder/field-view.js
+++ assets/js/src/resume-builder/field-view.js
@@ -65,20 +65,43 @@ function register_field_type_template( field_type, template_id ) {
 let Field_View = window.Backbone.View.extend( {
 	tagName: 'li',
 	wrap_template: '',
-	initialize: function() {
-		let wrap_el = document.getElementById( 'fz-resume-template-meta-field-wrap' );
-		this.wrap_template = Handlebars.compile( wrap_el.innerHTML );
-	},
-	render: function() {
-		this.$el.html( this.wrap_template() );
-
-		let field_type = this.model.get( 'field' );
-
-		this.$el.find( '.meta-field-inside').append( field_templates[ field_type ]( this.model.toJSON() ) );
-
-		return this;
-	}
+	initialize: field_view_initialize,
+	render: field_view_render
 } );
+
+/**
+ * The initialization function for Field Views, used in the Field_View Backbone View.
+ * Makes sure the View has a copy of the field wrap template.
+ *
+ * @summary The initialization function for Field Views.
+ *
+ * @since 0.1.0
+ * @access private
+ */
+function field_view_initialize() {
+	let wrap_el = document.getElementById( 'fz-resume-template-meta-field-wrap' );
+	this.wrap_template = Handlebars.compile( wrap_el.innerHTML );
+}
+
+/**
+ * The render function for Field Views, used in the Field_View Backbone View.
+ * Creates the field wrap and then fills it in with the template for the
+ * field type the model is for.
+ *
+ * @summary The render function for Field Views.
+ *
+ * @since 0.1.0
+ * @access private
+ */
+function field_view_render() {
+	this.$el.html( this.wrap_template() );
+
+	let field_type = this.model.get( 'field' );
+
+	this.$el.find( '.meta-field-inside').append( field_templates[ field_type ]( this.model.toJSON() ) );
+
+	return this;
+}
 
 export default {
 	register_field_type_template, Field_View
