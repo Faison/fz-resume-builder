2d28faf07a149b75d8e04190fedb8405e50b9969
Faison Zutavern <fzutavern@my.wctc.edu>
2016-12-08 21:13:21 -0600

    Removed a local variable for jQuery, since it's only used once

--- assets/js/src/admin.js
+++ assets/js/src/admin.js
@@ -5,9 +5,7 @@
 
 import resume_builder from './resume-builder/resume-builder';
 
-let $ = window.jQuery;
-
-$( document ).ready( function() {
+window.jQuery( document ).ready( function() {
 	let field_types = window.fz_resume_field_types;
 	let data = window.fz_resume_field_data;
 
